ac4a7547a6884c950e42d7f1150b71004504c15a
mohammadezzat93 <mohammadd9390@gmail.com>
2021-09-06 19:23:04 +0300

    Add Weather

---
+++ server.js
@@ -0,0 +1,63 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express'); // import express
+const WeatherData = require('./assests/Weather.json');
+const cors = require('cors'); //import
+
+const server = express();
+server.use(cors())
+
+const PORT = process.env.PORT;
+
+/* 
+we can request 3 ways : 
+1 : browser
+2 : from Extentsions vs : Thunder Client
+3 : Postman
+
+*/
+
+// http://localhost:3010/
+server.get('/', (req,res) =>{
+    res.send('Send 1');
+});
+
+// http://localhost:3000/test
+server.get('/test',(req,res) =>{
+res.send('Send 2');
+});
+
+// http://localhost:3000/clouds
+server.get('/clouds',(req,res) =>{
+    let description = WeatherData[0].data.map( item=> {
+        console.log(item.weather.description);
+        return [item.datetime,item.weather.description];
+        
+    });
+    res.send(description);
+    });
+
+    // http://localhost:3000/getPock?name=bulbasaur
+server.get('/getPock',(req,res) =>{
+    const description = req.query.description;
+    const x = WeatherData[0].data.find(item =>{
+        if(item.description === description)
+            return item;
+    });
+    res.send(x)
+    });
+
+ // uneversal : http://localhost:3000/******* */  **Always End**
+server.get('*',(req,res) =>{
+
+    res.status(404).send('Sory , Page Not found');
+    });
+
+
+
+server.listen(PORT, () =>{
+    console.log(`im listening on ${PORT}`);
+});
+
+
