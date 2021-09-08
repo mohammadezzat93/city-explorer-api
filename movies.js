ba46bc868534eb4eddec3e84f2e9bf2d41a0af27
mohammadezzat93 <mohammadd9390@gmail.com>
2021-09-08 22:24:29 +0300

    Finish Lab 09

---
+++ movies.js
@@ -0,0 +1,36 @@
+
+const axios=require('axios');
+
+
+function moduleMovie(req,res){ 
+    
+    let name = req.query.query;
+          
+    let URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.movie_APP_Key}&language=en-US&page=1&include_adult=false&query=${name}`;
+
+        axios
+        .get(URL)
+        .then (modulem => {
+            let newMovie = modulem.data.results.map((item) => {
+
+                return new movie(item);
+        })
+        res.send(newMovie);
+        })
+        .catch(()=>{
+            res.status(404).send('Sorry , Not found 404');
+        })
+
+    }
+
+
+    function movie(day) {
+      (this.title = day.original_title),
+        (this.overview = day.overview),
+        (this.average_votes = day.vote_average),
+        (this.total_votes = day.vote_count),
+        (this.popularity = day.popularity),
+        (this.released_on = day.release_date);
+    }
+
+    module.exports=moduleMovie;
---
+++ weather.js
@@ -0,0 +1,50 @@
+
+const axios=require('axios');
+
+// http://localhost:3010/weather?city_Name=Amman
+// server.get('/weather', (req, res) => {
+//     const cityName = req.query.cityName;
+
+//     let weatherArray =[]
+//     let weather = WeatherData.find( item=> {
+
+//         if (cityName=== item.city_name ){
+//             weatherArray = item.WeatherData.map((day) => {
+//                 let newObj = new Forecast(day);
+//                 return newObj;
+//               });
+//             }
+//           });
+//   res.send(weatherArray);
+// });
+
+// function Forecast(day) {
+//   (this.date = day.valid_date),
+//     (this.desc = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`);
+// }
+
+function moduleMovie(req,res){ 
+
+    let name = req.query.city;
+    let URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${name}&key=${process.env.Weather_APP_Key}`;
+    
+         axios
+         .get(URL)
+         .then(modulew =>{
+            let newDay = modulew.data.data.map((item) => {
+                return new Forecast(item);
+         })
+         res.send(newDay);
+            })
+            .catch(()=>{
+                res.status(404).send('Sorry , Not found 404');
+            })
+        
+    }
+
+    function Forecast(day) {
+        (this.date = day.valid_date),
+            (this.desc = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`);
+    }
+
+    module.exports=moduleMovie;
