fcf95764d023b676f3b271dbcc6c1bafdc5e3fac
mohammadezzat93 <mohammadd9390@gmail.com>
2021-09-09 16:27:57 +0300

    Finish lab 10

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # city-explorer-api
+
+**Author**: Mohammad Alhaj Ali
+**Version**: 1.0.5
+
+## Name of feature: memory
+
+## Estimate of time needed to complete: 20 min
+
+## Start time: 1.00 pm
+
+## Finish time: 1.20 pm
+
+## Actual time needed to complete: 20 min
--- movies.js
+++ movies.js
@@ -1,12 +1,21 @@
 
 const axios=require('axios');
+let myMemory={};
 
 
 function moduleMovie(req,res){ 
     
     let name = req.query.query;
-          
-    let URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.movie_APP_Key}&language=en-US&page=1&include_adult=false&query=${name}`;
+
+    if(myMemory[name]!==undefined)
+
+    {
+      res.send(myMemory[name]);
+    }
+       
+    else {
+
+        let URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.movie_APP_Key}&language=en-US&page=1&include_adult=false&query=${name}`;
 
         axios
         .get(URL)
@@ -15,11 +24,15 @@ function moduleMovie(req,res){
 
                 return new movie(item);
         })
-        res.send(newMovie);
+        myMemory[name]=newMovie;
+
+        res.status(200).send(newMovie);
         })
         .catch(()=>{
             res.status(404).send('Sorry , Not found 404');
         })
+
+    }
 
     }
 
